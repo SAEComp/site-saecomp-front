c954652d5bbe515cbc1e74cbeaf4d8d9ed4897b3
Gabriel B. Di Rezze <168911776+DiRezze@users.noreply.github.com>
2026-08-27 17:57:50 -0300

    fix: review workInProgress typo

--- src/pages/WorkInProgress/index.tsx
+++ src/pages/WorkInProgress/index.tsx
@@ -5,7 +5,7 @@ const WorkInProgress = () => {
         <section className="m-4 gap-8 flex flex-col flex-nowrap justify-center items-center text-[2.5rem]">
             <h1 className="text-center font-bold text-green-500">Ainda estamos trabalhando nisso...</h1>
             <img src={pelucia} className="max-h-60 max-w-60 object-contain" alt="Perry de pelúcia"/>
-            <h2 className="text-center text-[1rem] text-neutral-700">A SAEComp está dando o máxino no desenvolvimento desta seção. Acompanhe em breve!</h2>
+            <h2 className="text-center text-[1rem] text-neutral-700">A SAEComp está dando o máximo no desenvolvimento desta seção. Acompanhe em breve!</h2>
         
         </section>
     );
